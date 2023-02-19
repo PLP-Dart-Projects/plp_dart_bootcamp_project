2964154e850bbccbe1edefb19478511250d5282c
Lackson Munthali <munthalilackson20@gmail.com>
2023-02-20 01:31:27 +0200

    Notifications screen

---
+++ lib/screens/notifications.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+import '../widgets/notification_list.dart';
+
+class Notifications extends StatelessWidget {
+  const Notifications({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: const [
+        NotificationList(),
+      ],
+    );
+  }
+}
